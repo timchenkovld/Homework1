fa374f7c81ddaea58fad385560f11e80eb271111
BOSS <timchenko.vld@gmail.com>
2023-04-30 19:48:29 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        1. Вывести в одну строку символы:
+//
+//        for (char ch = 'A'; ch <= 'Z'; ch++) {
+//            System.out.print(ch);
+//        }
+//
+//        for (char ch1 = 'z'; 'a' <= ch1; ch1--) {
+//            System.out.print(ch1);
+//        }
+//
+//        for (char ch2 = 'а'; ch2 <= 'я'; ch2++){
+//            System.out.print(ch2);
+//        }
+
+//        for (byte i = 0; i <= 9; i++){
+//            System.out.print(i);
+//        }
+
+//        //  ●	печатного диапазона таблицы ASCII
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите символ: ");
+//        char ch = scanner.next().charAt(0);
+//        int ascii = (char) ch;
+//        System.out.println("Значение: " + ascii);
+
+      //  2. Написать и протестировать функции преобразования:
+
+//        int x = 1;
+//        String y = String.valueOf((int) x);
+//        System.out.println(y);
+
+//        float x = 123.2f;
+//        String y = String.valueOf((float) x);
+//        System.out.println(y);
+
+//        String st = "123";
+//        int x = Integer.parseInt((String) st);
+//        System.out.println(x);
+
+//        String st = "1234.34";
+//        float x = Float.parseFloat((String) st);
+//        System.out.println(x);
+
+        
+    }
+}
